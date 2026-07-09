026e1d1f2a4ea418388f2592a6f9aae1cc281b7d
Ali Bakhtiari <mukhtar2022bakhtiari>
2026-07-09 11:23:00 +0430

    fix: format and align columns in ps user program

--- user/ps.c
+++ user/ps.c
@@ -7,32 +7,30 @@ int main(int argc, char *argv[])
 {
     struct pinfo info;
     
-    // فراخوانی سیستم‌کالی که در گام‌های قبلی ساختیم
     if(getpinfo(&info) < 0) {
         printf("ps: failed to get process info\n");
         exit(1);
     }
 
-    // چاپ هدر جدول
+    // هدر جدول با دو تب بین STATE و PRIO
     printf("PID\tSTATE\t\tPRIO\tTICKETS\tNAME\n");
     
-    // حلقه برای چاپ اطلاعات تک‌تک فرآیندها
     for(int i = 0; i < info.proc_count; i++) {
         char *state_str;
         
-        // تبدیل وضعیت عددی فرآیند به متن قابل فهم
+        // هم‌اندازه کردن طول رشته‌ها به ۸ کاراکتر برای تنظیم دقیق تب‌ها
         switch(info.procs[i].state) {
-            case 0: state_str = "UNUSED"; break;
-            case 1: state_str = "USED"; break;
-            case 2: state_str = "SLEEPING"; break;
-            case 3: state_str = "RUNNABLE"; break;
-            case 4: state_str = "RUNNING"; break;
-            case 5: state_str = "ZOMBIE"; break;
-            default: state_str = "UNKNOWN"; break;
+            case 0: state_str = "UNUSED  "; break;
+            case 1: state_str = "USED    "; break;
+            case 2: state_str = "SLEEPING"; break; 
+            case 3: state_str = "RUNNABLE"; break; 
+            case 4: state_str = "RUNNING "; break; // اضافه کردن یک اسپیس برای هم‌سایز شدن با SLEEPING
+            case 5: state_str = "ZOMBIE  "; break; 
+            default: state_str = "UNKNOWN "; break;
         }
 
-        // چاپ اطلاعات به صورت ستونی
-        printf("%d\t%s\t\t%d\t%d\t%s\n", 
+        // دقت کن که فقط ۵ تا فرمت صادر شده و ۵ متغیر پاس داده شده است
+        printf("%d\t%s\t%d\t%d\t%s\n", 
                info.procs[i].pid, 
                state_str, 
                info.procs[i].priority, 
@@ -42,4 +40,3 @@ int main(int argc, char *argv[])
     
     exit(0);
 }
-
