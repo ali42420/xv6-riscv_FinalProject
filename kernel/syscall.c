ce50ac0f0d3ae2d27183c6a0fa8774fecbf7a926
Ali Bakhtiari <mukhtar2022bakhtiari>
2026-07-10 09:49:30 +0430

    kernel: map new syscalls to their core handlers

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -104,6 +104,7 @@ extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
 extern uint64 sys_sync(void);
 extern uint64 sys_getpinfo(void);   // added
+extern uint64 sys_setpriority(void);  // added_2
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -131,7 +132,9 @@ static uint64 (*syscalls[])(void) = {
   [SYS_mkdir]   sys_mkdir,
   [SYS_close]   sys_close,
   [SYS_sync]    sys_sync,
-  [SYS_getpinfo]    sys_getpinfo,  // added
+  [SYS_getpinfo]    sys_getpinfo,  
+  [SYS_setpriority]  sys_setpriority,
+  
   // clang-format on
 };
 
